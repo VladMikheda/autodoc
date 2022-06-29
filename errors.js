e89d266ab0521612644d5212a84229530567949a
VladMikheda <vladmikheda@gmail.com>
2022-06-29 11:11:03 +0200

    add errors function

--- errors.js
+++ errors.js
@@ -1,10 +1,29 @@
 
 const ERROR_ARGUMENTS = 1;
 const ERROR_PATCH = 2;
+const ERROR_READ_FILE = 3;
+
+const helpPrint = () => {
+    console.log("todo help")
+}
 
 
+const errorPrint = (error, text, help) => {
+    if(text){
+        console.error("ERROR: " + text);
+    }
+
+    if(help){
+        helpPrint();
+    }
+
+    process.exit(error);
+}
 
 module.exports = {
     ERROR_ARGUMENTS,
     ERROR_PATCH,
+    ERROR_READ_FILE,
+    errorPrint,
+    helpPrint
 }
